b6deba417f4fa209c23b5a8011a3b0baafbf1ac9
Mark Shamray <endeymus@gmail.com>
2019-09-07 21:37:17 +0300

    8 task 1 exercises

--- src/ExerciseOne.java
+++ src/ExerciseOne.java
@@ -5,9 +5,9 @@ public class ExerciseOne {
     public static void main(String[] args)
     {
         Primitiv prim1 = new Primitiv();
+        Primitiv prim2 = new Primitiv();
         Primitiv.increment();
         System.out.println("F: " + prim1.i);
-        prim1.increment();
-        System.out.println("Two: " + prim1.i);
+        System.out.println("Two: " + prim2.i);
     }
 }
